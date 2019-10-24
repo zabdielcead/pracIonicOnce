cae5df17280f8b1d72fd50d12615f8bae8397eef
zabdielcead <zabdiel.cead@gmail.com>
2019-10-23 19:34:09 -0500

    funcionalidad oopciones

--- leafletdir/src/app/tab1/tab1.page.ts
+++ leafletdir/src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { latLng, tileLayer, circle, polygon } from 'leaflet';
+import { latLng, tileLayer, circle, polygon, marker} from 'leaflet';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,8 +7,11 @@ import { latLng, tileLayer, circle, polygon } from 'leaflet';
 })
 export class Tab1Page implements OnInit {
   // https://github.com/Asymmetrik/ngx-leaflet
+  // https://github.com/Asymmetrik/ngx-leaflet-tutorial-plugins/tree/master/heatmap.js
   options = {};
   layersControl = {};
+  layers = {};
+
 
 
   constructor() {}
@@ -18,14 +21,14 @@ export class Tab1Page implements OnInit {
   }
 
   loadMap() {
-    this.options = {
+    this.options = {   // mapa
       layers: [
         tileLayer('https://cartocdn_{s}.global.ssl.fastly.net/base-eco/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
       ],
       zoom: 2,
       center: latLng(46.879966, -121.726909)
     };
-    this.layersControl = {
+    this.layersControl = {  // opciones del menu
       baseLayers: {
         'Open Street Map': tileLayer('https://cartocdn_{s}.global.ssl.fastly.net/base-eco/{z}/{x}/{y}.png',
          { maxZoom: 18, attribution: '...' }),
@@ -37,6 +40,12 @@ export class Tab1Page implements OnInit {
         'Big Square': polygon([[ 46.8, -121.55 ], [ 46.9, -121.55 ], [ 46.9, -121.7 ], [ 46.8, -121.7 ]])
       }
     };
+
+    this.layers = [
+      circle([ 46.95, -122 ], { radius: 5000 }),
+      polygon([[ 46.8, -121.85 ], [ 46.92, -121.92 ], [ 46.87, -121.8 ]]),
+      marker([ 46.879966, -121.726909 ])
+  ];
   }
 
 }
