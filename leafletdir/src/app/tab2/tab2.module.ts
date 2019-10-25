3b42868a4c54013c5f99f0726a83aea90e607454
zabdielcead <zabdiel.cead@gmail.com>
2019-10-25 15:22:10 -0500

    suben geojson

--- leafletdir/src/app/tab2/tab2.module.ts
+++ leafletdir/src/app/tab2/tab2.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    LeafletModule,
     RouterModule.forChild([{ path: '', component: Tab2Page }])
   ],
   declarations: [Tab2Page]
--- leafletdir/src/app/tab2/tab2.page.ts
+++ leafletdir/src/app/tab2/tab2.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { latLng, tileLayer, circle, polygon, marker, Map, geoJSON} from 'leaflet';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-
+export class Tab2Page  implements OnInit {
+  options = {};
   constructor() {}
+
+  ngOnInit() {
+    this.loadMapa();
+  }
+
+  loadMapa() {
+    this.options = {   // mapa
+      layers: [
+        tileLayer('https://cartocdn_{s}.global.ssl.fastly.net/base-eco/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      ],
+      zoom: 2,
+      center: latLng(37, -95)
+    };
+  }
+
+  onMapReady(map: Map) {
+    fetch('./assets/usas/data.json').then(res => res.json())
+    .then(json => {
+      console.log(json);
+      geoJSON(json).addTo(map);
+     });
+  }
 
 }
